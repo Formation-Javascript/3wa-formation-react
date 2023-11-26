cb6c1b38005cf798dae20db22b5d8e1562b86b74
MAMADOU SECK <mamadouseck@Mac-mini-de-MAMADOU.local>
2023-11-26 18:07:36 +0100

    done 🚀

--- src/App.js
+++ src/App.js
@@ -1,31 +1,108 @@
 import { useState } from 'react';
-import Start from './components/Start';
-import End from './components/End';
+import { RiFileCopyLine } from 'react-icons/ri';
+import { FaArrowRight } from 'react-icons/fa';
+import Radio from './components/Radio';
 
-// Définition du composant principal App
+const options = ['Include Uppercase Letters', 'Include Lowercase Letters', 'Include Numbers', 'Include Symbols'];
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+';
+
 export default function App() {
-  // État pour stocker la note sélectionnée
-  const [ratingSelected, setRatingSelected] = useState(null);
-  // État pour contrôler si la soumission a eu lieu
-  const [submitted, setSubmitted] = useState(false);
+  const [characterLength, setCharacterLength] = useState('0');
+  const [optionsSelected, setOptionsSelected] = useState([]);
+  const [password, setPassword] = useState('');
 
-  // Fonction pour sélectionner une note
-  const selectRating = (rating) => setRatingSelected(rating);
+  const onChangeCharacterLength = (e) => setCharacterLength(String(e.target.value));
 
-  // Fonction appelée lors de la soumission
-  const onSubmit = () => ratingSelected && setSubmitted(true);
+  const handleOptionsSelected = function (e) {
+    if (e.target.checked) {
+      setOptionsSelected([...optionsSelected, e.target.id]);
+    }
+  };
+
+  const generatePassword = () => {
+    let characters = '';
+    if (optionsSelected.includes('Include Uppercase Letters')) {
+      characters += uppercaseLetters;
+    }
+    if (optionsSelected.includes('Include Lowercase Letters')) {
+      characters += lowercaseLetters;
+    }
+    if (optionsSelected.includes('Include Numbers')) {
+      characters += numbers;
+    }
+    if (optionsSelected.includes('Include Symbols')) {
+      characters += symbols;
+    }
+
+    let password = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < characterLength; i++) {
+      password += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    setPassword(password);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(password);
+  };
+
 
   return (
-    // Conteneur principal de l'application
-    <main className="bg-black min-h-screen overflow-hidden flex justify-center items-center">
-      {/* Conteneur interne pour la section de notation */}
-      <div className="bg-gray-800 w-96 max-w-xl max-h-xl h-96 rounded-3xl shadow-l p-8 ">
-        {/* Affiche le composant End si soumis, sinon affiche le composant Start */}
-        {submitted ? (
-          <End />
-        ) : (
-          <Start selectRating={selectRating} ratingSelected={ratingSelected} onSubmit={onSubmit} />
-        )}
+    <main className="w-96 max-w-xl ">
+      <h1 className="text-gray-50 text-center mb-10">PassSafeMaker</h1>
+
+      <div className="flex flex-col space-y-6">
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={copyToClipboard} >
+            <RiFileCopyLine className="h-5 w-5 text-indigo-700" aria-hidden="true" />
+          </div>
+          <input
+            type="text"
+            className="block w-full rounded-md border-0 p-4 text-gray-900 placeholder:text-gray-400 text-lg"
+            placeholder="P4$5w0rD"
+            value={password}
+          />
+        </div>
+
+        <div className="bg-gray-50 rounded-lg p-4 flex flex-col space-y-6">
+          <div>
+            <div className="flex justify-between items-center">
+              <label htmlFor="minmax-range" className="block mb-2 text-sm font-medium text-gray-900">
+                Character Length
+              </label>
+
+              <p className="text-xl text-indigo-700">{characterLength}</p>
+            </div>
+            <input
+              onChange={onChangeCharacterLength}
+              type="range"
+              min="0"
+              max="20"
+              value={characterLength}
+              className="w-full h-2 bg-indigo-300 rounded-lg appearance-none cursor-pointer accent-indigo-700"
+            />
+          </div>
+
+          <div>
+            {options.map((option) => (
+              <Radio handleOptionsSelected={handleOptionsSelected} key={option} label={option} />
+            ))}
+          </div>
+
+          <button
+            disabled={characterLength === '0'}
+            onClick={generatePassword}
+            className={`uppercase w-full py-3 hover:bg-indigo-900 rounded text-gray-50 flex justify-center items-center space-x-5 
+            ${characterLength === '0' ? 'bg-indigo-700/50' : 'bg-indigo-700'}
+            `}>
+            <span>Generate </span>
+            <FaArrowRight />
+          </button>
+        </div>
       </div>
     </main>
   );
