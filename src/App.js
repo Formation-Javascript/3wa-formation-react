dfbbe10854a411781c5a4ef7df93dda8dd2606f1
MAMADOU SECK <mamadouseck@Mac-mini-de-MAMADOU.local>
2023-11-30 17:28:07 +0100

    create router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import Greeting from './components/Greeting';
-import ItemTopics from './components/ItemTopics';
-import data from './assets/data';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import QuizPage from './pages/QuizPage';
 
-function App() {
+export default function App() {
   return (
-    <main className="bg-slate-900 min-h-screen">
-      <div className="max-w-7xl mx-auto flex flex-col gap-14 md:grid md:grid-cols-3 justify-center items-center min-h-screen md:space-x-36 px-8">
-        <Greeting></Greeting>
-
-        <section className='md:col-span-2 '>
-          <ul className='w-full flex flex-col gap-5'>
-            {data.quizzes.map((item, index) => (
-              <ItemTopics
-                key={index}
-                title={item.title}
-                logo={item.icon}></ItemTopics>
-            ))}
-          </ul>
-        </section>
-      </div>
-    </main>
+    <Routes>
+      {/*👇 Chemin imbriqué */}
+      <Route path="/">
+        {/* Index permet de définir le composant a afficher sur le chemin `/` */}
+        <Route index element={<HomePage />} />
+        <Route path=":indexTopic" element={<QuizPage />} />
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
